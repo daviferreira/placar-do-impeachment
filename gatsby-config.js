e57f9d47d58925a991638d2fa9ca5680fe4096c5
Davi Ferreira <contato@daviferreira.com>
2021-01-16 14:41:04 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Placar do Impeachment",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+  ],
+};
